cee883caa1c53e7c2889f60d8dac0e4e66bce78a
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2022-06-20 10:59:27 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
+import addImage from "./add-image.js";
+
+const helloWorldBtn = new HelloWorldButton();
+helloWorldBtn.render();
+addImage();
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    // publicPath: 'http://the-most-awesome=website.com/',
+    publicPath: 'dist/',
+  },
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(css)$/,
+        use : [
+          'style-loader', 'css-loader'
+        ]
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use : [
+          'style-loader', 'css-loader', 'sass-loader'
+        ]
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+            plugins: ['transform-class-properties'],
+          }
+        }
+      }
+    ]
+  }
+}
